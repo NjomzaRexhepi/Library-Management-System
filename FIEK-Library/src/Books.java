f05cdd338aaa91e4a2b2030b10d8d1ee237a4895
Njomza Rexhepi <75095687+NjomzaRexhepi@users.noreply.github.com>
2022-05-27 16:20:37 +0200

    books backend

---
+++ FIEK-Library/src/Books.java
@@ -0,0 +1,18 @@
+public class Books {
+    private String title;
+    private String author;
+    private String category;
+    private String shelfID;
+    private int available;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }

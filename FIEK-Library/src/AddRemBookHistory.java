486e7370e9677a2799bb44a82bd4e75c63490cc3
Dielllza1 <75162762+Dielllza1@users.noreply.github.com>
2022-05-29 13:15:45 +0200

    add book

--- FIEK-Library/src/AddRemBookHistory.java
+++ FIEK-Library/src/AddRemBookHistory.java
@@ -20,6 +20,70 @@ public class AddRemBookHistory {
         this.addedByName=addedByName;
         this.amount=amount;
     }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getAct() {
+        return act;
+    }
+
+    public void setAct(String act) {
+        this.act = act;
+    }
+
+    public int getAddedByID() {
+        return addedByID;
+    }
+
+    public void setAddedByID(int addedByID) {
+        this.addedByID = addedByID;
+    }
+
+    public String getAddedByName() {
+        return addedByName;
+    }
+
+    public void setAddedByName(String addedByName) {
+        this.addedByName = addedByName;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 
   
 }

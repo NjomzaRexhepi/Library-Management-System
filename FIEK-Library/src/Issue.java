16ba65907613d62b887432474b00386a37aa40e1
Dielllza1 <75162762+Dielllza1@users.noreply.github.com>
2022-05-29 13:05:29 +0200

    issue add commit

--- FIEK-Library/src/Issue.java
+++ FIEK-Library/src/Issue.java
@@ -73,23 +73,6 @@ public class Issue {
             memberID.clear();
         });
         
-          confirm.setOnAction(event -> {
-
-            ConnectionClass mycon=ConnectionClass.getInstance();
-            try {
-                mycon.IssueBook(ID,title.getText(),author.getText(),shelfID.getText(),amount.getText(), memberID.getText());
-                title.clear();
-                author.clear();
-                shelfID.clear();
-                amount.clear();
-                memberID.clear();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-        });
-
-        return issueBox;
-    }
+         
     
 }

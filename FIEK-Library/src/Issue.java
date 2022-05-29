8ad39c21a59aeb79efeb4d722ede91d10a845cde
Dielllza1 <75162762+Dielllza1@users.noreply.github.com>
2022-05-29 13:03:25 +0200

    Issue commit add

--- FIEK-Library/src/Issue.java
+++ FIEK-Library/src/Issue.java
@@ -19,5 +19,50 @@ public class Issue {
         }
         return obj;
     }
+    public VBox getIssueWindow(int ID, String password){
+
+        TextField title = new TextField();
+        title.setPrefSize(300, 55);
+        title.setMaxSize(300, 55);
+        title.setPromptText("Title");
+        title.setFocusTraversable(false);
+        TextField author = new TextField();
+        author.setPrefSize(300, 55);
+        author.setMaxSize(300, 55);
+        author.setPromptText("Author");
+        author.setFocusTraversable(false);
+        TextField shelfID = new TextField();
+        shelfID.setPrefSize(300, 55);
+        shelfID.setMaxSize(300, 55);
+        shelfID.setPromptText("Shelf-ID");
+        shelfID.setFocusTraversable(false);
+        TextField memberID = new TextField();
+        TextField amount = new TextField();
+        amount.setPrefSize(300, 55);
+        amount.setMaxSize(300, 55);
+        amount.setPromptText("Amount");
+        amount.setFocusTraversable(false);
+        memberID.setPrefSize(300, 55);
+        memberID.setMaxSize(300, 55);
+        memberID.setPromptText("Member ID");
+        memberID.setFocusTraversable(false);
+
+        Button confirm = new Button("Confirm");
+        confirm.setPrefSize(120, 25);
+        confirm.setMaxSize(120, 25);
+        Button clear = new Button("Clear");
+        clear.setPrefSize(120, 25);
+        clear.setMaxSize(120, 25);
+        HBox hbox=new HBox();
+        hbox.setAlignment(Pos.CENTER);
+        hbox.setSpacing(20);
+        hbox.getChildren().addAll(clear, confirm);
+
+        VBox issueBox= new VBox();
+        issueBox.setAlignment(Pos.CENTER);
+        issueBox.getChildren().addAll(title, author, shelfID, amount, memberID, hbox);
+        issueBox.setSpacing(30);
+        issueBox.setPrefWidth(330);
+        issueBox.setMinWidth(330);
     
 }

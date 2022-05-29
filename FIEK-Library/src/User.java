3959dc5ef504cc6098bd14e796a4e2b9f20236ad
Njomza Rexhepi <75095687+NjomzaRexhepi@users.noreply.github.com>
2022-05-29 12:36:39 +0200

    user be

---
+++ FIEK-Library/src/User.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class User {
+    private static int nbUser = 0;
+    private int userId;
+    private PrintStream streamOut;
+    private InputStream streamIn;
+    private String nickname;
+    private Socket client;
+    private String color;
+
+    public User(){}
+
+    public User(String name) {
+        this.nickname=name;
+    }
+   
+}

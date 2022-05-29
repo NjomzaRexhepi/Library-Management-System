0d1fe5a04070d10c4c568ae97737cf968a27e1fc
Njomza Rexhepi <75095687+NjomzaRexhepi@users.noreply.github.com>
2022-05-29 21:09:00 +0200

    Update User.java

--- FIEK-Library/src/User.java
+++ FIEK-Library/src/User.java
@@ -17,19 +17,30 @@ public class User {
     public User(String name) {
         this.nickname=name;
     }
-
-    private static int nbUser = 0;
-    private int userId;
-    private PrintStream streamOut;
-    private InputStream streamIn;
-    private String nickname;
-    private Socket client;
-    private String color;
-
-    public User(){}
-
-    public User(String name) {
-        this.nickname=name;
+    // constructor
+    public User(Socket client, String name) throws IOException {
+        this.streamOut = new PrintStream(client.getOutputStream());
+        this.streamIn = client.getInputStream();
+        this.client = client;
+        this.nickname = name;
+        this.userId = nbUser;
+        this.color = "";
+        nbUser += 1;
     }
-   
+
+    public PrintStream getOutStream(){
+        return this.streamOut;
+    }
+
+    public InputStream getInputStream(){
+        return this.streamIn;
+    }
+
+    public String getNickname(){
+        return this.nickname;
+    }
+
+    public String toString(){
+        return this.nickname;
+    }
 }

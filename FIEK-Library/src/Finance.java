f13f6f0b813a66e0953e482a52a0b8e307e54f30
Gentrim Canolli <74964656+gentrimcanolli@users.noreply.github.com>
2022-05-29 13:28:27 +0200

    Update Finance.java

--- FIEK-Library/src/Finance.java
+++ FIEK-Library/src/Finance.java
@@ -105,3 +105,71 @@ public class Finance {
 
                 });
             });
+        
+setMembershipFee.setOnAction(event -> {
+
+            ConnectionClass con=ConnectionClass.getInstance();
+            String []vals=new String[4];
+            try {
+                vals=con.getMisc("Membership Fee");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            Label current= new Label("Current Status:");
+            current.setStyle("-fx-font-size: 14;" +
+                    " -fx-text-fill: green;");
+            Label fee= new Label();
+            fee.setStyle("-fx-font-size: 14;" +
+                    " -fx-text-fill: green;");
+            fee.setText("Membership Fee: "+vals[0]);
+            Label date= new Label();
+            date.setStyle("-fx-font-size: 14;" +
+                    " -fx-text-fill: green;");
+            date.setText("Last updated on: "+vals[3]);
+            Label updateID= new Label();
+            updateID.setStyle("-fx-font-size: 14;" +
+                    " -fx-text-fill: green;");
+            updateID.setText("Last updated by: "+vals[2]);
+            TextField membershipFee=new TextField();
+            membershipFee.setPromptText("Enter Membership Fee per Month");
+            membershipFee.setPrefSize(300,55);
+            membershipFee.setMaxSize(300,55);
+
+            Button confirm = new Button("Confirm");
+            confirm.setPrefSize(120, 25);
+            confirm.setMaxSize(120, 25);
+            Button back = new Button("Back");
+            back.setPrefSize(120, 25);
+            back.setMaxSize(120, 25);
+            HBox hbox=new HBox();
+            hbox.setAlignment(Pos.CENTER);
+            hbox.setSpacing(20);
+            hbox.getChildren().addAll(back, confirm);
+
+            VBox setSalaryBox= new VBox();
+            setSalaryBox.setAlignment(Pos.CENTER);
+            setSalaryBox.getChildren().addAll(current, fee, date, updateID, membershipFee, hbox);
+            setSalaryBox.setSpacing(30);
+            setSalaryBox.setPrefWidth(330);
+            setSalaryBox.setMinWidth(330);
+
+            fullBox.getChildren().clear();
+            fullBox.getChildren().add(setSalaryBox);
+
+            back.setOnAction(event1 -> {
+                fullBox.getChildren().clear();
+                fullBox.getChildren().addAll(leftBox, rightBox);
+                fullBox.setAlignment(Pos.CENTER);
+                fullBox.setSpacing(100);
+            });
+            confirm.setOnAction(event1 -> {
+                try {
+                    con.setMisc("Membership Fee", membershipFee.getText(), "null", id);
+                    membershipFee.setStyle("-fx-background-color: #98FB98");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    membershipFee.setStyle("-fx-background-color: #FFB6C1");
+                }
+            });
+            });

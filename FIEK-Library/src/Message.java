3b5cdac2e9018d7cf3105c753ed3622a2c74e8e2
erblinaberisha <75163819+erblinaberisha@users.noreply.github.com>
2022-05-29 11:31:39 +0200

    Add files via upload

---
+++ FIEK-Library/src/Message.java
@@ -0,0 +1,13 @@
+public class Message {
+    public String content;
+
+    public Message() {}
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String toString() {
+        return this.content;
+    }
+}

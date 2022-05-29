a04f9f70dd6ee55e394d04218af75433ee1140b2
nidaislami <75091196+nidaislami@users.noreply.github.com>
2022-05-29 12:55:48 +0200

    Update EmployeeFinanceTable.java

--- FIEK-Library/src/EmployeeFinanceTable.java
+++ FIEK-Library/src/EmployeeFinanceTable.java
@@ -20,3 +20,67 @@ public class EmployeeFinanceTable {
         this.date=date;
         this.time=time;
     }
+ public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getOccasion() {
+        return occasion;
+    }
+
+    public void setOccasion(String occasion) {
+        this.occasion = occasion;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getEnteredBy() {
+        return enteredBy;
+    }
+
+    public void setEnteredBy(int enteredBy) {
+        this.enteredBy = enteredBy;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
